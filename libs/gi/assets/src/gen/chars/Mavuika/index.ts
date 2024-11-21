5688a73e7e8aea32a4cfb3066f9c424079cb67f9
Ryan <mineminemine@users.noreply.github.com>
2024-11-21 23:33:22 +0800

    char fixes

--- libs/gi/assets/src/gen/chars/Mavuika/index.ts
+++ libs/gi/assets/src/gen/chars/Mavuika/index.ts
@@ -6,14 +6,14 @@ import iconSide from './UI_AvatarIcon_Side_Shougun.png'
 import bar from './UI_NameCardPic_Shougun_Alpha.png'
 import banner from './UI_NameCardPic_Shougun_P.png'
 import constellation1 from './UI_Talent_S_Mavuika_01.png'
-import constellation4 from './UI_Talent_S_Mavuika_02.png'
-import constellation2 from './UI_Talent_S_Mavuika_03.png'
+import constellation2 from './UI_Talent_S_Mavuika_02.png'
+import constellation4 from './UI_Talent_S_Mavuika_03.png'
 import constellation6 from './UI_Talent_S_Mavuika_04.png'
 import passive1 from './UI_Talent_S_Mavuika_05.png'
 import passive2 from './UI_Talent_S_Mavuika_06.png'
 import passive3 from './UI_Talent_S_Mavuika_07.png'
-import constellation5 from './UI_Talent_U_Mavuika_01.png'
-import constellation3 from './UI_Talent_U_Mavuika_02.png'
+import constellation3 from './UI_Talent_U_Mavuika_01.png'
+import constellation5 from './UI_Talent_U_Mavuika_02.png'
 
 const data = {
   banner,
